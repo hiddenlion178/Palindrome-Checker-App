30ddcd74fca7ceea8abd6cb7366415dad8833e5f
hiddenlion178 <hiddenlion17@gmail.com>
2026-02-23 10:21:16 +0530

    UC2:Hardcoded palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the Palindrome Checker Management System");
+        String word = "madam";
+        boolean isPalindrome = true;
 
-        System.out.println("Version : 1.0");
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
 
-        System.out.println("System initialized successfully.");
-
-
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome.");
+        } else {
+            System.out.println(word + " is NOT a Palindrome.");
+        }
     }
 }
